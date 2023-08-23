cd00864957f8e015357ac479c4c32c5b72bb50ca
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:28:36 +0300

    " "

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                         <p>Welcome to Laravel Crud Application</p>
                     </div>
                   <br>
-                 <a href="product" class="btn btn-primary">Show Products</a>
+                 <a href="product" class="btn btn-primary ">Show Products</a>
                 </div>
             </div>
         </div>
