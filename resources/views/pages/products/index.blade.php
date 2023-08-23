a7258612245c3c321bd84ebf91518024bf0e0cc5
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:10:46 +0300

    "t"

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Laravel CRUD Applic</h1>
+                <h1>Laravel CRUD Applicat</h1>
             </div>
         </div>
     </div>
