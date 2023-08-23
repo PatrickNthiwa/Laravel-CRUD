67d8df0d2be510e3c54b32aa6bcbd73870bac531
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:28:57 +0300

    "ub"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                         <p>Welcome to Laravel Crud Application</p>
                     </div>
                   <br>
-                  b
+                  ub
                 </div>
             </div>
         </div>
