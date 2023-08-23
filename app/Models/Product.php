7fe00e94df44c6075460dbb48c693b864841215f
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:01:04 +0300

    "''"

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,5 +9,5 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable =['title','description','short_notes','price','imag']
+    protected $fillable =['title','description','short_notes','price','image','']
 }
