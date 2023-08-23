3f73162ee23e4f988581f799b6f3a33eb1ce884a
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:11:41 +0300

    "r"

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -6,6 +6,7 @@
             <div class="col-12 text-center pt-5">
                 <h1>Laravel CRUD Application</h1>
                 <p>Welcome to Laravel Project</p>
+                br
             </div>
         </div>
     </div>
