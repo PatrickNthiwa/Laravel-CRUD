f235c3c479d0277bc77b0249cb9ecd9ce2ed0126
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:11:10 +0300

    "e"

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12 text">
+            <div class="col-12 text-ce">
                 <h1>Laravel CRUD Application</h1>
             </div>
         </div>
