36ed3dd7b871647a9ca5f703769b3a13a96fcfba
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:28:16 +0300

    "n"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100 ">
        
                     <div class="row text-center pt-5 " >
-                        <h1  >This is a Laravel CRUD Application for Begi.</h1>
+                        <h1  >This is a Laravel CRUD Application for Begiin.</h1>
                         <p>Welcome to Laravel Crud Application</p>
                     </div>
                   <br>
