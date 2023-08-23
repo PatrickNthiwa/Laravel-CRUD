4f3647d41e5a7c5c157ff60f48364989e4cd8177
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:26:16 +0300

    ""

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100  text-cente">
+                <div class="p-6 text-gray-900 dark:text-gray-100  text-cen">
                   <h1  >Laravel CRUD Application.</h1>
 
                   <p>Welcome to Laravel Crud Application</p>
