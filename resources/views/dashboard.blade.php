e80a6b7f3d1de41a90c864955b72813fa9f33141
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:23:56 +0300

    "r"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                   <h1>Laravel CRUD Application.</h1>
 
-                  <p>Welcome to Laravel </p>
+                  <p>Welcome to Laravel Cr</p>
                 </div>
             </div>
         </div>
