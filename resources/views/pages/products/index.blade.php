80eae15c45c38095580c43d84c5db1d00405bdcb
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:12:04 +0300

    "h"

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -7,7 +7,7 @@
                 <h1>Laravel CRUD Application</h1>
                 <p>Welcome to Laravel Project</p>
                 <br>
-                <a href="produc"></a>
+                <a href="product">Show</a>
             </div>
         </div>
     </div>
