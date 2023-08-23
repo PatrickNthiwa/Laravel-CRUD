c06f73778bf9777aa405a7bd727386486bf3957d
PatrickNthiwa <pwnthiwa@gmail.com>
2023-08-23 17:10:25 +0300

    ""

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -3,11 +3,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <text-center>
-                    <pt-5></pt-5>
-                </text-center>
-            </div>
+            .col-12>text-center>pt-
         </div>
     </div>
 @endsection
